38c8fa727918d1cccb4d6f2ac681efa0c7a78957
Manuel Prieto <eduardolopez31102005@gmail.com>
2024-03-10 13:00:51 -0400

    version 1.3

--- Practicas con JavaScript/app.js
+++ Practicas con JavaScript/app.js
@@ -1,12 +1,7 @@
-let typeCard = 'Debid Card';
 
-switch(typeCard) {
-	case 'Debid Card' :
-		console.log("this is a debid card");
-		break;
-	case 'Credit Card' :
-		console.log("this is a credit card");
-		break;
-	default:
-		console.log("no card");
+let count = 0;
+
+while(count <= 50) {
+	console.log(count);
+	count++;
 }
