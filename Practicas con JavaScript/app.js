c1d4fc19cca3e909eb25ad62da0d0d73c97b2d91
Manuel Prieto <eduardolopez31102005@gmail.com>
2024-03-11 14:47:10 -0400

    basico de de JavaScript en Programacion Orientada a Objetos

--- Practicas con JavaScript/app.js
+++ Practicas con JavaScript/app.js
@@ -1,15 +1,1 @@
-const user = {
-	name: 'joe',
-	age: 30
-}
-
-function printInfo(user) {
-	const {name, age} = user;
-
-	console.log(name, age);
-	return '<h1>hola ' + user.name + '</h1>';
-}
-
-console.log(printInfo(user));
-
-document.body.innerHTML = printInfo(user);
+console.log("hello world 2");
