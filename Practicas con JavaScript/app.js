a124d893dd2e74a6c565a780306d6aee2710a938
Manuel Prieto <eduardolopez31102005@gmail.com>
2024-03-11 15:10:05 -0400

    Objeto Literal en JavaScript

--- Practicas con JavaScript/app.js
+++ Practicas con JavaScript/app.js
@@ -1,1 +1,17 @@
-console.log("hello world 2");
+console.log( { } );
+
+let name = 'ryan';
+let lastname = 'ray';
+let age = 30;
+
+const user = {
+	name: 'ryan',
+	lastname: 'ray',
+	age: 30,
+	hobbies: ['read', 'programming', 'run'],
+	address: {
+		street: 'some street',
+		city:'london',
+	}
+}
+console.log(user);
