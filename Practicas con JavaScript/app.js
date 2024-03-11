02ca087cf3ee300ee4d13468ce27a587d4c37c1f
Manuel Prieto <eduardolopez31102005@gmail.com>
2024-03-11 15:42:27 -0400

    Metodos en JavaScript en POO

--- Practicas con JavaScript/app.js
+++ Practicas con JavaScript/app.js
@@ -1,5 +1,17 @@
+function showFullName(){
+	return 'Ryan Ray'
+}
+
 const user = {
 	name: 'ryan', //properties
 	lastname: 'ray', //properties
-	age: 30 //properties
+	age: 30, //properties
+	showFullName() {
+		return 'ryan ray'
+	}
 }
+console.log(user.showFullName());
+
+//toda funcion que este dentro de 
+//un objeto (Object)
+//se convierte en metodo (Methods)
