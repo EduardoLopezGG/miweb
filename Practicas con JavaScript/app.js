681c7fcafcdec6043e54e6bf702b9f2e5e7e2594
Manuel Prieto <eduardolopez31102005@gmail.com>
2024-03-10 21:14:11 -0400

    objetos en JavaScript y Reactjs

--- Practicas con JavaScript/app.js
+++ Practicas con JavaScript/app.js
@@ -1,6 +1,19 @@
-function greeting(){
-	console.log("hello");
+const user = {
+	name : 'ryan',
+	lastname : 'perez',
+	age : 20,
+	address : {
+		country : 'colombia',
+		city : 'bogota',
+		street : 'main street 123',
+	},
+	friends : ['brandon', 'elena'],
+	active : true,
+	sendMail : function (){
+		return 'sending email...';
+	}
 }
-
-greeting();
-greeting();
+console.log(user.address.city);
+console.log(user.friends);
+console.log(user.active);
+console.log(user.sendMail())
