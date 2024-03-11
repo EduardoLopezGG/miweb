472525b3899013a350b056dd5dcb47050b8ca51b
Manuel Prieto <eduardolopez31102005@gmail.com>
2024-03-11 16:00:58 -0400

    This en JavaScript

--- Practicas con JavaScript/app.js
+++ Practicas con JavaScript/app.js
@@ -3,11 +3,11 @@ function showFullName(){
 }
 
 const user = {
-	name: 'ryan', //properties
-	lastname: 'ray', //properties
+	name: 'pepe', //properties
+	lastname: 'Perez', //properties
 	age: 30, //properties
 	showFullName() {
-		return 'ryan ray'
+		return this.name + " " + this.lastname
 	}
 }
 console.log(user.showFullName());
@@ -15,3 +15,23 @@ console.log(user.showFullName());
 //toda funcion que este dentro de 
 //un objeto (Object)
 //se convierte en metodo (Methods)
+
+const account = {
+	number : "123456789",
+	amount: 100,
+	deposit(quantity) {
+		this.amount = this.amount + quantity
+	},
+	withdraw(quantity) {
+		this.amount = this.amount - quantity
+	}
+}
+
+account.deposit(100)
+account.deposit(10)
+account.deposit(50)
+console.log(account)
+
+account.withdraw(10)
+account.withdraw(200)
+console.log(account)
