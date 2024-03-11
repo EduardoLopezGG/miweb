de325a3c023d998451d6b2fdb0dc4da045ef7153
Manuel Prieto <eduardolopez31102005@gmail.com>
2024-03-10 21:28:59 -0400

    manipulacion del DOM

--- Practicas con JavaScript/app.js
+++ Practicas con JavaScript/app.js
@@ -1,9 +1,13 @@
-const name = 'laptop';
-const price = 3000;
+const title = document.createElement('h1')
+title.innerText = 'hola mundo';
 
-const newProduct = {
-	name,
-	price
-}
+const button = document.createElement('button');
+button.innerText = 'click'
 
-console.log(newProduct);
+button.addEventListener('click', function(){
+	title.innerText = 'texto actualizado con Js';
+	alert('se realizo un click');
+})
+
+document.body.append(title);
+document.body.append(button);
