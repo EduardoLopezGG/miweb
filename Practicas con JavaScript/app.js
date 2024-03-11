68e9fbbfe171472c1c4a612f0585774dcd19cc36
Manuel Prieto <eduardolopez31102005@gmail.com>
2024-03-10 21:16:31 -0400

    waos

--- Practicas con JavaScript/app.js
+++ Practicas con JavaScript/app.js
@@ -1,19 +1,9 @@
-const user = {
-	name : 'ryan',
-	lastname : 'perez',
-	age : 20,
-	address : {
-		country : 'colombia',
-		city : 'bogota',
-		street : 'main street 123',
-	},
-	friends : ['brandon', 'elena'],
-	active : true,
-	sendMail : function (){
-		return 'sending email...';
-	}
+const name = 'laptop';
+const price = 3000;
+
+const newProduct = {
+	name,
+	price
 }
-console.log(user.address.city);
-console.log(user.friends);
-console.log(user.active);
-console.log(user.sendMail())
+
+console.log(newProduct);
