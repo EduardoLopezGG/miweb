0489bcd0709769f52f8adc7a24989b013cf5a90a
Manuel Prieto <eduardolopez31102005@gmail.com>
2024-03-12 19:53:03 -0400

    Polimorfismo - Sobrecarga en JavaScript

--- Practicas con JavaScript/app.js
+++ Practicas con JavaScript/app.js
@@ -1,22 +1,12 @@
-class Person {
-	constructor(name, lastname){
-		this.name = name
-		this.lastname = lastname
-		this.age = null
-	}
+function countItems(x) {
+	return x.toString().length
+}
+console.log(countItems(10000))
+console.log(countItems("hola mundo"))
+
+function sumar(x = 0, y = 0, z = 0) {
+	return x + y + z
 }
 
-class Programmer extends Person{
-	constructor(name, lastname, language){
-		super(name, lastname);
-		this.language = language
-	}
-}
-
-const person = new Person("maria", "perez")
-
-console.log(person)
-
-const programmer = new Programmer("joe", "mcmillan", "python")
-
-console.log(programmer)
+console.log(sumar(10, 20))
+console.log(sumar(10, 20, 30))
