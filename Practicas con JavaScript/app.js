1faff4e411bd9a91da97ee8faf7efe02fc7ddc2a
Manuel Prieto <eduardolopez31102005@gmail.com>
2024-03-12 18:54:27 -0400

    Encapsulacion en JavaScript

--- Practicas con JavaScript/app.js
+++ Practicas con JavaScript/app.js
@@ -1,9 +1,31 @@
 const person = {
-	name: "ryan",
-	lastname: "ray",
-	address: {
-		street: "123 baker street",
-		city: "london",
-		country: "united kingdom"
+	name: "fazt tech",
+	empleados: [],
+	sortempleados: function(){}
+}
+
+function Company (name){
+	let employees = []
+	this.name = name
+
+	this.getEmployees = function() {
+		return employees
+	}
+
+	this.addEmployees = function(employee){
+		employees.push(employee)
 	}
 }
+const company = new Company("coca cola")
+const company2 = new Company("pepsi cola")
+
+console.log(company)
+console.log(company2)
+
+company.addEmployees({name: "ryan"})
+company2.addEmployees({name: "joe"})
+
+console.log(company.getEmployees())
+console.log(company2.getEmployees())
+
+console.log(Company())
