47aaa451dda28e0fadd5edc0b56e34918f5d1e24
Manuel Prieto <eduardolopez31102005@gmail.com>
2024-03-12 17:18:54 -0400

    Clases en JavaScript

--- Practicas con JavaScript/app.js
+++ Practicas con JavaScript/app.js
@@ -1,6 +1,17 @@
-function Person() {
-	this.name = ""
-	this.lastname = ""
+class Person{
+	constructor(name, lastname){
+		this.name = name
+		this.lastname = lastname
+	}
+
+	greet() {
+		return "hello i am " + this.name + " " + this.lastname
+	}
+
 }
-const person = new Person()
-console.log(person)
+
+const user = new Person("joe", "mcmillan")
+const user2 = new Person("maria", "juana")
+
+console.log(user.greet())
+console.log(typeof user2.greet())
