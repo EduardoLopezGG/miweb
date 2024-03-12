25ff88f089ced27c51ae252ea633eb85ccf1f1f9
Manuel Prieto <eduardolopez31102005@gmail.com>
2024-03-12 15:48:27 -0400

    Object Constructor

--- Practicas con JavaScript/app.js
+++ Practicas con JavaScript/app.js
@@ -1,14 +1,9 @@
-function Person(){
-	this.name = ""
-	this.lastname = ""
-	this.age = 0
-	this.showFullName = function(){
-		return this.name + " "	+ this.lastname
-	}
+const user = {
+	name: "ryan",
+	lastname: "ray",
+	age: 30,
+	showname(){
+		return this.name;
+	},
 }
-
-const user1 = new Person()
-user1.name = "joe"
-user1.lastname = "mcmillan"
-console.log(user1.showFullName())
-console.log(user1)
+console.log(Object.values(user))
