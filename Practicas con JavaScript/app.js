30bf75ecc6a071c829663688ee7cb2268233dba1
Manuel Prieto <eduardolopez31102005@gmail.com>
2024-03-12 20:17:12 -0400

    SubType Polimorfism en Javascript

--- Practicas con JavaScript/app.js
+++ Practicas con JavaScript/app.js
@@ -1,16 +1,26 @@
-function Stack(){
-	this.items = []
-
-	this.push = function(item) {
-		this.items.push(item)
+class Person{
+	constructor(name, lastname){
+		this.name = name
+		this.lastname = lastname		
 	}
 }
 
-const stack = new Stack()
-stack.push("asdfasdfasdfads")
+class Programmer extends Person {
+	constructor(language, name, lastname) {
+		super(name, language);
+		this.language = language
+	}
+}
 
-const stack2 = new Stack()
-stack2.push(10000)
+const john = new Person("john", "ray")
+const ryan = new Programmer("JavaScript", "ryan", "ray")
 
-console.log(stack)
-console.log(stack2)
+console.log(john)
+console.log(ryan)
+
+function writeFullName (p) {
+	console.log(p.name + " " + p.lastname)
+}
+
+writeFullName(john)
+writeFullName(ryan)
