99be9915c4bd087265c90fe57d12669334a0a6cc
Manuel Prieto <eduardolopez31102005@gmail.com>
2024-03-14 15:02:05 -0400

    Operadores en Python

---
+++ Practicas con Python/02_operators.py
@@ -0,0 +1,33 @@
+### Operadores ###
+
+### Operadores Aritmeticos ###
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 % 2)
+print(10 // 3)
+print(2 ** 3)
+
+print("hola " + "python" + "¿que tal?")
+print("hola " + str(5))
+print("hola " * (2 ** 3))
+
+my_int = 2.5 * 2
+print("hola " * int(my_int))
+
+### Operadores Comparativos ###
+
+print(3 < 4)
+print(3 > 4)
+print(3 <= 4)
+print(3 >= 4)
+print(3 == 4)
+print(3 != 4)
+
+### Operadores Logicos ###
+
+print(3 > 4 and "hola" > "python")
+print(3 > 4 or "hola" > "python")
+print(3 > 4 not "hola" > "python")
