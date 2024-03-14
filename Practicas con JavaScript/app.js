85424762352d4212129bf430fce73fb47e607ada
Manuel Prieto <eduardolopez31102005@gmail.com>
2024-03-14 10:30:37 -0400

    ultimos detalles de JavaScript

--- Practicas con JavaScript/app.js
+++ Practicas con JavaScript/app.js
@@ -1,26 +1,22 @@
-class Person{
-	constructor(name, lastname){
-		this.name = name
-		this.lastname = lastname		
-	}
+const ul = document.createElement("ul")
+
+let date = fetch("https://jsonplaceholder.typicode.com/posts")
+	.then(function(response){
+		return response.json()
+	}).then(function(data){
+		console.log(data)
+		data.forEach(function(post){
+			const li = document.createElement("li");
+			li.innerText = post.title;
+			ul.append(li);
+		})
+		document.body.append(ul);
+	})
+
+async function loadData() {
+	const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+	const data = await response.json
+	console.log(data)
 }
-
-class Programmer extends Person {
-	constructor(language, name, lastname) {
-		super(name, language);
-		this.language = language
-	}
-}
-
-const john = new Person("john", "ray")
-const ryan = new Programmer("JavaScript", "ryan", "ray")
-
-console.log(john)
-console.log(ryan)
-
-function writeFullName (p) {
-	console.log(p.name + " " + p.lastname)
-}
-
-writeFullName(john)
-writeFullName(ryan)
+loadData()
+console.log("linea 2")
