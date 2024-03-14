3d5f35d0c3aad53a9222a3dc92f7fce097069427
Manuel Prieto <eduardolopez31102005@gmail.com>
2024-03-14 14:01:47 -0400

    Variables en Python

---
+++ Practicas con Python/01_variables.py
@@ -0,0 +1,39 @@
+#variables
+
+my_string_variable = "mi variable string"
+print(my_string_variable)
+ 
+my_int_variable = 5
+print(my_int_variable)
+
+my_bool_variable = False
+print(my_bool_variable)
+
+#algunas funciones del sistema
+
+print(len(my_string_variable))
+
+#variables en una sola linea ¡Cuidado con abusar de esta sintaxis!
+
+name, surname, alias, age = "manuel", "prieto", "manugameplays", 30
+print(name, surname, alias, age)
+
+#inputs
+
+first_name = input("¿cual es tu nombre? ")
+age = input("¿Cuantos años tienes? ")
+
+print(name)
+print(age)
+
+#cambiamos su tipo
+
+name = 35
+age = "brais"
+print(name)
+print(age)
+
+#forzamos el tipo de dato
+address = str = "mi direccion"
+address = 32
+print(address)
