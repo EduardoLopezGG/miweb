f82754737bddb7787524e9a2a969eb925517ab81
Manuel Prieto <eduardolopez31102005@gmail.com>
2024-03-24 10:26:42 -0400

    funciones

--- Practicas con Python/10_functions.py
+++ Practicas con Python/10_functions.py
@@ -1,1 +1,14 @@
 ### Funciones ###
+
+def my_function ():
+	print("esto es una funcion")
+
+my_function()
+my_function()
+my_function()
+my_function()
+
+def sum_two_values (first_number, second_number):
+	print(first_number + second_number)
+
+sum_two_values(5, 7)
